79b25d2b694dbdc8e10e9cf43a719b065f43005b
Jos <jos.gesenhues@cbc.de>
2020-04-08 17:29:31 +0200

    all working

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # sf_crime
-Udacity Spark streaming
+Udacity Spark Streaming
+
+Please find the nScreenshots in `screenshots.zip` and note that I did add a screenshot of the SQL-Tab of the Spark-UI instead of the deprecated Streaming tab.
+
+Answers to the questions:
+1. How did changing values on the SparkSession property parameters affect the throughput and latency of the data?
+
+Without any parameter tuning, the throuput of my application is 
+```JSON
+  "numInputRows" : 200,
+  "inputRowsPerSecond" : 15.85791309863622,
+  "processedRowsPerSecond" : 19.201228878648234,
+  "durationMs" : {
+    "addBatch" : 10236,
+    "getBatch" : 0,
+    "getEndOffset" : 1,
+    "queryPlanning" : 77,
+    "setOffsetRange" : 2,
+    "triggerExecution" : 10416,
+    "walCommit" : 52
+  }
+```
+Reducing `spark.sql.shuffle.partitions` and `spark.default.parallelism` to `1` and  (since this "cluster" has only one executor) lead to
+```JSON
+  "numInputRows" : 200,
+  "inputRowsPerSecond" : 14.726456078344746,
+  "processedRowsPerSecond" : 293.6857562408223,
+  "durationMs" : {
+    "addBatch" : 445,
+    "getBatch" : 0,
+    "getEndOffset" : 1,
+    "queryPlanning" : 72,
+    "setOffsetRange" : 7,
+    "triggerExecution" : 681,
+    "walCommit" : 90
+  }
+```
+Setting `spark.executor.memory` and `spark.driver.memory` to `4g` led to an further (small) speedup:
+```JSON
+  "numInputRows" : 200,
+  "inputRowsPerSecond" : 15.006002400960385,
+  "processedRowsPerSecond" : 300.7518796992481,
+  "durationMs" : {
+    "addBatch" : 444,
+    "getBatch" : 1,
+    "getEndOffset" : 0,
+    "queryPlanning" : 65,
+    "setOffsetRange" : 8,
+    "triggerExecution" : 665,
+    "walCommit" : 85
+  }
+```
+Setting `spark.streaming.kafka.minRatePerPartition` to `400` led to 
+```JSON
+  "numInputRows" : 200,
+  "inputRowsPerSecond" : 6.666888896296544,
+  "processedRowsPerSecond" : 396.03960396039605,
+  "durationMs" : {
+    "addBatch" : 320,
+    "getBatch" : 0,
+    "getEndOffset" : 0,
+    "queryPlanning" : 57,
+    "setOffsetRange" : 3,
+    "triggerExecution" : 505,
+    "walCommit" : 70
+  }
+```
+2. What were the 2-3 most efficient SparkSession property key/value pairs? Through testing multiple variations on values, how can you tell these were the most optimal?
+
+As seen above, adapting the properties that are related to the actual cluster size were the most efficient.
+
+After that, setting `spark.streaming.kafka.minRatePerPartition` was very efficient. I tried different values, but `400` seems to be a sweet spot.
+
+As learned in the course, increasing `spark.executor.memory` and `spark.driver.memory` often have an effect, but since the data handled here is rather small, this effect was not too big.
--- data_stream.py
+++ data_stream.py
@@ -1,75 +1,113 @@
 import logging
-import json
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-import pyspark.sql.functions as psf
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+import pyspark.sql.functions as func
 
 
-# TODO Create a schema for incoming resources
-schema = StructType([
-])
+schema = StructType(
+    [
+        StructField("crime_id", StringType()),
+        StructField("original_crime_type_name", StringType()),
+        StructField("report_date", TimestampType()),
+        StructField("call_date", TimestampType()),
+        StructField("offense_date", TimestampType()),
+        StructField("call_time", StringType()),
+        StructField("call_date_time", TimestampType()),
+        StructField("disposition", StringType()),
+        StructField("address", StringType()),
+        StructField("city", StringType()),
+        StructField("state", StringType()),
+        StructField("agency_id", StringType()),
+        StructField("address_type", StringType()),
+        StructField("common_location", StringType()),
+    ]
+)
+
 
 def run_spark_job(spark):
-
-    # TODO Create Spark Configuration
-    # Create Spark configurations with max offset of 200 per trigger
-    # set up correct bootstrap server and port
-    df = spark \
-        .readStream \
-
+    df = (
+        spark.readStream.format("kafka")
+        .option("subscribe", "org.sf.police.calls")
+        .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", 200)
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .load()
+    )
     # Show schema for the incoming resources for checks
-    df.printSchema()
+    # df.printSchema()
 
-    # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
 
-    service_table = kafka_df\
-        .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
-        .select("DF.*")
+    service_table = kafka_df.select(
+        func.from_json(func.col("value"), schema).alias("DF")
+    ).select("DF.*")
 
-    # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    # select original_crime_type_name and disposition
+    distinct_table = service_table.select("original_crime_type_name", "disposition")
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table.groupBy("disposition").agg(
+        func.count("original_crime_type_name").alias("original_crime_types")
+    )
 
-    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
-    # TODO write output stream
-    query = agg_df \
+    # Q1. Submit a screen shot of a batch ingestion of the aggregation
+    # write output stream
+    query = (
+        agg_df.writeStream
+        .trigger(processingTime="30 second")
+        .outputMode("Complete")
+        .format("console")
+        .option("truncate", "false")
+        .start()
+    )
 
-
-    # TODO attach a ProgressReporter
-    query.awaitTermination()
-
-    # TODO get the right radio code json path
-    radio_code_json_filepath = ""
-    radio_code_df = spark.read.json(radio_code_json_filepath)
+    # get the right radio code json path
+    radio_code_json_filepath = "radio_code.json"
+    radio_code_df = spark.read.option("multiline", "true").json(
+        radio_code_json_filepath
+    )
 
     # clean up your data so that the column names match on radio_code_df and agg_df
     # we will want to join on the disposition code
-
-    # TODO rename disposition_code column to disposition
+    # rename disposition_code column to disposition
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
-    # TODO join on disposition column
-    join_query = agg_df.
-
-
+    # join on disposition column
+    join_query = (
+        agg_df.join(radio_code_df, on="disposition")
+        .writeStream.format("console")
+        .trigger(processingTime="30 second")
+        .outputMode("Complete")
+        .option("truncate", "false")
+        .start()
+    )
+    # attach a ProgressReporter
     join_query.awaitTermination()
+    query.awaitTermination()
 
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
-
-    # TODO Create Spark in Standalone mode
-    spark = SparkSession \
-        .builder \
-        .master("local[*]") \
-        .appName("KafkaSparkStructuredStreaming") \
+    conf = (
+        SparkConf()
+        .setMaster("local[*]")
+        .set(key="spark.sql.shuffle.partitions", value="1")
+        .set(key="spark.default.parallelism", value="1")
+        .set(key="spark.executor.memory", value="4g")
+        .set(key="spark.driver.memory", value="4g")
+        .set(key="spark.streaming.receiver.minRatePerPartition", value="400")
+    )
+    spark = (
+        SparkSession.builder.master("local[*]")
+        .config(conf=conf)
+        .appName("KafkaSparkStructuredStreaming")
         .getOrCreate()
+    )
 
     logger.info("Spark started")
+    # spark.sparkContext.setLogLevel("WARN")
 
     run_spark_job(spark)
 
